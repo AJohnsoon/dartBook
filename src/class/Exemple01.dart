2cf73eb3db764ff6c6bea65f7a9810333faccacd
Allan Johnson <allanjohnson.leal@gmail.com>
2021-09-29 22:19:35 -0300

    feature: intro in OOP

--- src/class/Exemple01.dart
+++ src/class/Exemple01.dart
@@ -6,6 +6,17 @@ class Person {
   late String status;
   late bool haveChildren;
 
+  Person() {}
+
+  Person.wekeUp(this.name, this.year) {
+    print("${name} weke up");
+    gMorning();
+  }
+
+  void gMorning() {
+    print("$name say: 'Good Morning'");
+  }
+
   void canTalk() {
     print("${name} say:  'Helo world'! ");
   }
