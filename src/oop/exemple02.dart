75a874feeaa257bbc5dd54bfb8f1fcad7ed19a41
Allan Johnson <allanjohnson.leal@gmail.com>
2021-09-29 22:50:17 -0300

    feature: intro in OOP - Get/Set

--- src/oop/exemple02.dart
+++ src/oop/exemple02.dart
@@ -4,6 +4,6 @@ void main() {
   Person person = new Person("AJohnsoon", 28, 'Married', true);
   person.makeBirthday();
   print(person);
-  Person baby = Person.wekeUp("Theo Henrique", 1);
+  Person baby = Person.wekeUp("Thew Henrique", 1);
   print(baby.name);
 }
