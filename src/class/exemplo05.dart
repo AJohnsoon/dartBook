f9018259ade525a867eac74a192de5d77cc6fb39
Allan Johnson <allanjohnson.leal@gmail.com>
2021-10-03 00:53:50 -0300

    exemple to abstract class

--- src/class/exemplo05.dart
+++ src/class/exemplo05.dart
@@ -5,3 +5,23 @@ class Exemple {
     return value;
   }
 }
+
+abstract class ExempleAbstract {
+  late String name;
+  late int value;
+
+  ExempleAbstract(this.name, this.value);
+
+  void Sound();
+}
+
+class AbstractExemple extends ExempleAbstract {
+  AbstractExemple(name, value) : super(name, value);
+
+  @override
+  void Sound() {
+    print("Sound: 'FoooooOnnnNN' ");
+  }
+
+  String toString() => ">>> Data <<<  Name: ${name} Value: ${value}";
+}
--- src/oop/exemple05.dart
+++ src/oop/exemple05.dart
@@ -5,4 +5,8 @@ void main() {
   const exempleConstFinal = "how is the differ between Final and Const";
   print(exemple);
   print(Exemple.Diff(exempleConstFinal));
+
+  AbstractExemple abstractEx = new AbstractExemple('Exemple', 13);
+  abstractEx.Sound();
+  print(abstractEx);
 }
